f224e4bf8e5bbf788a2566679f72a49563df6bf5
Akatsuki Division <125817831+borusara@users.noreply.github.com>
2023-07-01 10:08:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("6314151246:AAH2y2IVfhVpA_ZkkUuE9mGGXrTAAAgFgqo")
+APP_ID = int(os.environ.get("27565584"))
+API_HASH = os.environ.get("bb9e0e8f7da356036f981bd41697703c")
 
 youtube_next_fetch = 0  # time in minute
 
